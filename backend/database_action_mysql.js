1e59320bd6e27ba235c807366a7ee4d012a14be1
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2019-02-02 16:03:20 +0300

    Backend: nodemon introduced with start-dev command

--- backend/database_action_mysql.js
+++ backend/database_action_mysql.js
@@ -2,14 +2,15 @@ import mysql from 'mysql';
 
 import config from './config.js';
 
-export default class databaseActionMySQL {
+class databaseActionMySQL_ {
+
 	constructor(){
-		this.connection = mysql.createConnection(config.databaseParameters);
+
 	}
 
-	execute_select_(p_sql, p_parameters_as_array = []) {
+	execute_select_(p_db_conn, p_sql, p_parameters_as_array = []) {
 		return new Promise(resolve => {
-			this.connection.query(p_sql, p_parameters_as_array,
+			p_db_conn.query(p_sql, p_parameters_as_array,
 				(err, rows, fields) => {
 					if (err) throw err;
 					resolve(rows);
@@ -19,15 +20,16 @@ export default class databaseActionMySQL {
 	}
 
 	async execute_select(p_sql, p_parameters_as_array = []) {
-		this.connection.connect();
-		var l_retval_as_array_of_hashes = await execute_select_(p_sql, p_parameters_as_array);
-		this.connection.end();
+		let l_db_conn = mysql.createConnection(config.databaseParameters);
+		l_db_conn.connect();
+		var l_retval_as_array_of_hashes = await execute_select_(l_db_conn, p_sql, p_parameters_as_array);
+		l_db_conn.end();
 		return l_retval_as_array_of_hashes;
 	}
 
-	execute_updatedeleteinsert_(p_sql, p_parameters_as_array = []) {
+	execute_updatedeleteinsert_(p_db_conn, p_sql, p_parameters_as_array = []) {
 		return new Promise(resolve => {
-			this.connection.query(p_sql, p_parameters_as_array,
+			p_db_conn.query(p_sql, p_parameters_as_array,
 				(err, rows, fields) => {
 					if (err) throw err;
 					resolve(rows);
@@ -37,10 +39,16 @@ export default class databaseActionMySQL {
 	}
 
 	async execute_updatedeleteinsert(p_sql, p_parameters_as_array = []) {
-		this.connection.connect();
-		var l_retval_as_array_of_hashes = await execute_select_(p_sql, p_parameters_as_array);
-		this.connection.end();
+		let l_db_conn = mysql.createConnection(config.databaseParameters);
+		l_db_conn.connect();
+		var l_retval_as_array_of_hashes = await execute_select_(l_db_conn, p_sql, p_parameters_as_array);
+		l_db_conn.end();
 		return "OK";
 	}
 
-}
+};
+
+//All to use the single instance of this library - singleton pattern
+const databaseActionMySQL = new databaseActionMySQL_();
+
+export default databaseActionMySQL;
--- backend/dist/database_action_mysql.js
+++ backend/dist/database_action_mysql.js
@@ -18,22 +18,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var databaseActionMySQL = function () {
-	function databaseActionMySQL() {
-		_classCallCheck(this, databaseActionMySQL);
-
-		this.connection = _mysql2.default.createConnection(_config2.default.databaseParameters);
+var databaseActionMySQL_ = function () {
+	function databaseActionMySQL_() {
+		_classCallCheck(this, databaseActionMySQL_);
 	}
 
-	_createClass(databaseActionMySQL, [{
+	_createClass(databaseActionMySQL_, [{
 		key: 'execute_select_',
-		value: function execute_select_(p_sql) {
-			var _this = this;
-
-			var p_parameters_as_array = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+		value: function execute_select_(p_db_conn, p_sql) {
+			var p_parameters_as_array = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
 
 			return new Promise(function (resolve) {
-				_this.connection.query(p_sql, p_parameters_as_array, function (err, rows, fields) {
+				p_db_conn.query(p_sql, p_parameters_as_array, function (err, rows, fields) {
 					if (err) throw err;
 					resolve(rows);
 				});
@@ -44,20 +40,19 @@ var databaseActionMySQL = function () {
 		value: async function execute_select(p_sql) {
 			var p_parameters_as_array = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-			this.connection.connect();
-			var l_retval_as_array_of_hashes = await execute_select_(p_sql, p_parameters_as_array);
-			this.connection.end();
+			var l_db_conn = _mysql2.default.createConnection(_config2.default.databaseParameters);
+			l_db_conn.connect();
+			var l_retval_as_array_of_hashes = await execute_select_(l_db_conn, p_sql, p_parameters_as_array);
+			l_db_conn.end();
 			return l_retval_as_array_of_hashes;
 		}
 	}, {
 		key: 'execute_updatedeleteinsert_',
-		value: function execute_updatedeleteinsert_(p_sql) {
-			var _this2 = this;
-
-			var p_parameters_as_array = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+		value: function execute_updatedeleteinsert_(p_db_conn, p_sql) {
+			var p_parameters_as_array = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
 
 			return new Promise(function (resolve) {
-				_this2.connection.query(p_sql, p_parameters_as_array, function (err, rows, fields) {
+				p_db_conn.query(p_sql, p_parameters_as_array, function (err, rows, fields) {
 					if (err) throw err;
 					resolve(rows);
 				});
@@ -68,14 +63,20 @@ var databaseActionMySQL = function () {
 		value: async function execute_updatedeleteinsert(p_sql) {
 			var p_parameters_as_array = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-			this.connection.connect();
-			var l_retval_as_array_of_hashes = await execute_select_(p_sql, p_parameters_as_array);
-			this.connection.end();
+			var l_db_conn = _mysql2.default.createConnection(_config2.default.databaseParameters);
+			l_db_conn.connect();
+			var l_retval_as_array_of_hashes = await execute_select_(l_db_conn, p_sql, p_parameters_as_array);
+			l_db_conn.end();
 			return "OK";
 		}
 	}]);
 
-	return databaseActionMySQL;
+	return databaseActionMySQL_;
 }();
+
+;
+
+//All to use the single instance of this library - singleton pattern
+var databaseActionMySQL = new databaseActionMySQL_();
 
 exports.default = databaseActionMySQL;
