387a2d7006b6557699733ff6a4b46b60e6b20765
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2019-02-07 00:45:40 +0300

    Frontend Native: YourApplication component introduced.

---
+++ frontend-native/components/YourApplication.js
@@ -0,0 +1,139 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+import {types} from '../redux-store.js';
+
+import { Image } from 'react-native';
+import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+
+const cards = [
+	{
+		text: 'Your Application Comes Here!',
+		name: 'This is the [YourApplication.js] file.',
+		image: require('../images/Car.gif'),
+	 },
+  ];
+
+const colors = [
+	"#C0C0C0",
+	"#808080",
+	"#FF0000",
+	"#800000",
+	"#808000",
+	"#00FF00",
+	"#00FFFF",
+	"#FF00FF",
+	"#800080",
+];
+
+function mapDispatchToProps(dispatch) {
+	return({
+		makered: () => {dispatch({type: types.HOMESCREENRED})},
+		makeblue: () => {dispatch({type: types.HOMESCREENBLUE})},
+		setAppState: (p_new_active_component) => {dispatch({
+			type: types.LOGINNAV,
+			activeLoginComponent: p_new_active_component
+		})},
+	})
+};
+
+function mapStateToProps(state) {
+	return({
+		homeScreenColorFromUp: state.homeScreenColor,
+		hostMachine: state.hostMachine,
+	});
+};
+
+class YourApplication extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			colorIndex: 0
+		};
+	};
+
+	componentDidMount(){
+		setInterval(() => {
+			l_index = (this.state.colorIndex + 1) % colors.length;
+			this.setState({colorIndex: l_index})
+		}, 1000);
+		console.log(this.props.hostMachine);
+	}
+
+	
+	
+	render() {
+		let l_bgcolor = colors[this.state.colorIndex];
+		return <Container>
+			<View style={{backgroundColor: l_bgcolor }}>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<DeckSwiper
+				dataSource={cards}
+				renderItem={item =>
+					<Card style={{ elevation: 3 }}>
+						<CardItem>
+							<Left>
+								<Thumbnail source={item.image} />
+								<Body>
+									<Text>{item.text}</Text>
+									<Text note>You can directly wire your application here.</Text>
+								</Body>
+							</Left>
+						</CardItem>
+						<CardItem cardBody>
+							<Image style={{ height: 300, flex: 1 }} source={item.image} />
+						</CardItem>
+						<CardItem>
+							<Icon name="heart" style={{ color: '#ED4A6A' }} />
+							<Text>{item.name}</Text>
+						</CardItem>
+					</Card>
+				}
+			/>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+			<Text></Text>
+
+			</View>
+		</Container>;
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(YourApplication);
--- frontend-native/screens/HomeScreen.js
+++ frontend-native/screens/HomeScreen.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import {
-	Image,
-	Platform,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
-
-import { Container, Header, Left, Body, Right, Title, Subtitle } from 'native-base';
 
 import { connect } from 'react-redux';
 
-import {types, loginComponents} from '../redux-store.js';
+import {types} from '../redux-store.js';
 
+import YourApplication from '../components/YourApplication.js';
 
 function mapDispatchToProps(dispatch) {
 	return({
@@ -38,221 +25,16 @@ function mapStateToProps(state) {
 };
 
 class HomeScreen extends React.Component {
-	static navigationOptions = {
-		header: null,
-	};
 
 	constructor(props){
 		super(props);
 		this.state = {
 		};
 	};
-
-	componentDidMount(){
-		setInterval(() => {
-			if (this.props.homeScreenColorFromUp == 'red') this.props.makeblue();
-			else this.props.makered();
-		}, 1000);
-		console.log(this.props.hostMachine);
-	}
-
-	async componentWillMount() {
-		await Expo.Font.loadAsync({
-			'Roboto': require('native-base/Fonts/Roboto.ttf'),
-			'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-		});
-	}
-		
+	
 	render() {
-		return <View style={styles.container}>
-			<ScrollView style={
-				this.props.homeScreenColorFromUp == 'red' 
-				? styles.containerRed
-				: styles.containerBlue
-			} contentContainerStyle={styles.contentContainer}>
-				<View style={styles.welcomeContainer}>
-					<Image
-						source={
-							__DEV__
-								? require('../assets/images/robot-dev.png')
-								: require('../assets/images/robot-prod.png')
-						}
-						style={styles.welcomeImage}
-					/>
-				</View>
-
-				<View style={styles.getStartedContainer}>
-					{this._maybeRenderDevelopmentModeWarning()}
-
-					<Text style={styles.getStartedText}>Get started by opening</Text>
-
-					<View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-						<MonoText style={styles.codeHighlightText}>
-							screens/HomeScreen.js
-						</MonoText>
-					</View>
-
-					<Text style={styles.getStartedText}>
-						Change this text and your app will automatically reload.
-					</Text>
-
-					<Text style={styles.getColorText}>
-						Identified host machine is {this.props.hostMachine}.
-					</Text>
-					<Text style={styles.getColorText}>
-						Current homescreen color is {this.props.homeScreenColorFromUp}.
-					</Text>
-
-
-				</View>
-
-				<View style={styles.helpContainer}>
-					<TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-						<Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-
-			<View style={styles.tabBarInfoContainer}>
-				<Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-				<View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-					<MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-				</View>
-			</View>
-
-		</View>;
+		return <YourApplication />;
 	}
-
-	_maybeRenderDevelopmentModeWarning() {
-		if (__DEV__) {
-			const learnMoreButton = (
-				<Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-					Learn more
-        </Text>
-			);
-
-			return (
-				<Text style={styles.developmentModeText}>
-					Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-				</Text>
-			);
-		} else {
-			return (
-				<Text style={styles.developmentModeText}>
-					You are not in development mode, your app will run at full speed.
-        </Text>
-			);
-		}
-	}
-
-	_handleLearnMorePress = () => {
-		WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-	};
-
-	_handleHelpPress = () => {
-		WebBrowser.openBrowserAsync(
-			'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-		);
-	};
 }
-
-const styles = StyleSheet.create({
-	containerRed: {
-		flex: 1,
-		backgroundColor: '#f00',
-	},
-	containerBlue: {
-		flex: 1,
-		backgroundColor: '#00f',
-	},
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-	developmentModeText: {
-		marginBottom: 20,
-		color: 'rgba(0,0,0,0.4)',
-		fontSize: 14,
-		lineHeight: 19,
-		textAlign: 'center',
-	},
-	contentContainer: {
-		paddingTop: 30,
-	},
-	welcomeContainer: {
-		alignItems: 'center',
-		marginTop: 10,
-		marginBottom: 20,
-	},
-	welcomeImage: {
-		width: 100,
-		height: 80,
-		resizeMode: 'contain',
-		marginTop: 3,
-		marginLeft: -10,
-	},
-	getStartedContainer: {
-		alignItems: 'center',
-		marginHorizontal: 50,
-	},
-	homeScreenFilename: {
-		marginVertical: 7,
-	},
-	codeHighlightText: {
-		color: 'rgba(96,100,109, 0.8)',
-	},
-	codeHighlightContainer: {
-		backgroundColor: 'rgba(0,0,0,0.05)',
-		borderRadius: 3,
-		paddingHorizontal: 4,
-	},
-	getStartedText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		lineHeight: 24,
-		textAlign: 'center',
-	},
-	tabBarInfoContainer: {
-		position: 'absolute',
-		bottom: 0,
-		left: 0,
-		right: 0,
-		...Platform.select({
-			ios: {
-				shadowColor: 'black',
-				shadowOffset: { height: -3 },
-				shadowOpacity: 0.1,
-				shadowRadius: 3,
-			},
-			android: {
-				elevation: 20,
-			},
-		}),
-		alignItems: 'center',
-		backgroundColor: '#fbfbfb',
-		paddingVertical: 20,
-	},
-	tabBarInfoText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		textAlign: 'center',
-	},
-	navigationFilename: {
-		marginTop: 5,
-	},
-	helpContainer: {
-		marginTop: 15,
-		alignItems: 'center',
-	},
-	helpLink: {
-		paddingVertical: 15,
-	},
-	helpLinkText: {
-		fontSize: 14,
-		color: '#2e78b7',
-	},
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
