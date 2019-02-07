ca9ab6a15d0bfed518639db6f291d76f52bbbe15
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2019-02-07 15:34:38 +0300

    gitignore dist

--- backend/request_handlers.js
+++ backend/request_handlers.js
@@ -310,6 +310,44 @@ export default class requestHandlers {
 
 	}
 
+	async updateData(p_req, p_res){
+		if (checkLock(p_req.ip) == "LOCKED") return p_res.json(config.signalsFrontendBackend.locked);
+		await incrementLockCount(p_req.ip);
+
+		let l_gender = p_req.body.gender;
+		let l_birthday = p_req.body.birthday;
+		let l_phone = p_req.body.phone;
+		let l_name = p_req.body.name;
+		let l_midname = p_req.body.midname;
+		let l_surname = p_req.body.surname;
+
+		if (validations.gender(l_gender) != "OK")return p_res.json(config.signalsFrontendBackend.genderTValidationFailed);
+		if (validations.birthday(l_birthday) != "OK") return p_res.json(config.signalsFrontendBackend.birthdayValidationFailed);
+		if (validations.phone(l_phone) != "OK") return p_res.json(config.signalsFrontendBackend.phoneValdiationFailed);
+
+		let l_email = "";
+		let l_jwt_payload = await validations.checkJWT(p_req);
+		if( nvl(l_jwt_payload["email"], "x") == "x" ) return p_res.json(config.signalsFrontendBackend.tokenNotValid);
+		else l_email = l_jwt_payload["email"];
+
+		let l_params = [];
+		l_params.push(l_name);
+		l_params.push(l_midname);
+		l_params.push(l_surname);
+		l_params.push(l_gender);
+		l_params.push(l_birthday);
+		l_params.push(l_phone);
+		l_params.push(l_email);
+		let l_result = await databaseActionMySQL.execute_updatedeleteinsert(sqls.updateData, l_params);
+		if (l_result == "OK") {
+			resetLockCount(p_req.ip, l_email);
+			return p_res.json(config.signalsFrontendBackend.updateDataSuccessful);
+		}
+		else {
+			return p_res.json(config.signalsFrontendBackend.updateDataFailed);
+		}
+	}
+
 	async updateEMail(p_req, p_res){
 		let l_email = p_req.body.email.toLowerCase();
 		if (checkLock(p_req.ip, l_email) == "LOCKED") return p_res.json(config.signalsFrontendBackend.locked);
@@ -351,7 +389,7 @@ export default class requestHandlers {
 		if (validations.password(l_password, l_password2) != "OK") return p_res.json(config.signalsFrontendBackend.passwordStrengthTestFailed);
 
 		let l_email = "";
-		let l_jwt_payload = validations.checkJWT(p_req);
+		let l_jwt_payload = await validations.checkJWT(p_req);
 		if( nvl(l_jwt_payload["email"], "x") == "x" ) return p_res.json(config.signalsFrontendBackend.tokenNotValid);
 		else l_email = l_jwt_payload["email"];
 
@@ -376,44 +414,6 @@ export default class requestHandlers {
 				return p_res.json(config.signalsFrontendBackend.updatePasswordFailed);
 			}
 		});
-	}
-
-	async updateData(p_req, p_res){
-		if (checkLock(p_req.ip) == "LOCKED") return p_res.json(config.signalsFrontendBackend.locked);
-		await incrementLockCount(p_req.ip);
-
-		let l_gender = p_req.body.gender;
-		let l_birthday = p_req.body.birthday;
-		let l_phone = p_req.body.phone;
-		let l_name = p_req.body.name;
-		let l_midname = p_req.body.midname;
-		let l_surname = p_req.body.surname;
-
-		if (validations.gender(l_gender) != "OK")return p_res.json(config.signalsFrontendBackend.genderTValidationFailed);
-		if (validations.birthday(l_birthday) != "OK") return p_res.json(config.signalsFrontendBackend.birthdayValidationFailed);
-		if (validations.phone(l_phone) != "OK") return p_res.json(config.signalsFrontendBackend.phoneValdiationFailed);
-
-		let l_email = "";
-		let l_jwt_payload = await validations.checkJWT(p_req);
-		if( nvl(l_jwt_payload["email"], "x") == "x" ) return p_res.json(config.signalsFrontendBackend.tokenNotValid);
-		else l_email = l_jwt_payload["email"];
-
-		let l_params = [];
-		l_params.push(l_name);
-		l_params.push(l_midname);
-		l_params.push(l_surname);
-		l_params.push(l_gender);
-		l_params.push(l_birthday);
-		l_params.push(l_phone);
-		l_params.push(l_email);
-		let l_result = await databaseActionMySQL.execute_updatedeleteinsert(sqls.updateData, l_params);
-		if (l_result == "OK") {
-			resetLockCount(p_req.ip, l_email);
-			return p_res.json(config.signalsFrontendBackend.updateDataSuccessful);
-		}
-		else {
-			return p_res.json(config.signalsFrontendBackend.updateDataFailed);
-		}
 	}
 
 	allOtherURLs(p_req, p_res){
